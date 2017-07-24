1e32b58f698c55ef40027b66f1c9481777b87bfe
GGysh <george.shy0806@gmail.com>
2017-07-24 11:10:06 -0700

    person

---
+++ PerSon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package per.son;
+
+/**
+ *
+ * @author Education Unlmited
+ */
+public class PerSon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("George");
+        System.out.println("14"); 
+        System.out.println("CN");
+        System.out.println("reading books, playing sports, studying physics");
+        System.out.println("blue and black ");
+    }
+    
+}
